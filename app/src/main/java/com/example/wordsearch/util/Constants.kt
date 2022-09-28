2d32953e5df19e5c5eecb05606f53fbbc706853f
James Henshaw <jimmy.hensh@gmail.com>
2022-09-28 17:02:45 +0100

    add constants

---
+++ app/src/main/java/com/example/wordsearch/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.wordsearch.util
+
+object Constants {
+    const val MAX_NO_OF_TURNS = 10
+    const val EASY = 10
+    const val MEDIUM = 20
+    const val HARD = 30
+    const val difficulty = HARD
+
+}
