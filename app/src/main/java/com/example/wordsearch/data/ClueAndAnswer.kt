19d88550ada45ab287b1d43d596dad742dfdaaf8
James Henshaw <jimmy.hensh@gmail.com>
2022-09-28 17:01:26 +0100

    add clues and answers as list of pairs

---
+++ app/src/main/java/com/example/wordsearch/data/ClueAndAnswer.kt
@@ -0,0 +1,10 @@
+package com.example.wordsearch.data
+
+// Set with all the words for the Game
+val allCluesAndAnswers: List<Pair<String,String>> =
+    listOf(
+        Pair("Famous pizza topping","pepperoni"),
+        Pair("Smelly cheese","limburger"),
+        Pair("Hot continent","africa"),
+        Pair("Capital of Hawaii","honolulu"),
+    )
