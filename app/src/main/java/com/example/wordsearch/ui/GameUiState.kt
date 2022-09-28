c1b11b070e3512e1c3bf271fd885b462b0372463
James Henshaw <jimmy.hensh@gmail.com>
2022-09-28 17:07:21 +0100

    create basic working version with skip word functionality

--- app/src/main/java/com/example/wordsearch/ui/GameUiState.kt
+++ app/src/main/java/com/example/wordsearch/ui/GameUiState.kt
@@ -1,5 +1,17 @@
 package com.example.wordsearch.ui
 
+
+
 data class GameUiState(
-    val currentJumbledWord: String = ""
-)
+    val currentClue: String = "",
+    val currentAnswer: String = "",
+    val currentJumble: String = "",
+    val wordInsertionIndex: Int = 0,
+    val wordSelectionIndex: Int = 0,
+    val generatedString: String = "",
+    val jumbleWithInsertedString: String = "",
+
+    val isGuessedWordWrong: Boolean = false,
+    val wordsCompleted: Int = 0,
+    val skipsPerformed: Int = 0
+    )
--- app/src/main/java/com/example/wordsearch/ui/WordSearch.kt
+++ app/src/main/java/com/example/wordsearch/ui/WordSearch.kt
@@ -1,17 +1,24 @@
 package com.example.wordsearch.ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.wordsearch.ui.GameViewModel
 
 @Composable
 fun WordSearch(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    gameViewModel: GameViewModel = viewModel(),
 ) {
-    //TODO <game ui state goes here>
+    val gameUiState by gameViewModel.uiState.collectAsState()
     var text by remember { mutableStateOf("") }
 
     Column (
@@ -26,21 +33,32 @@ fun WordSearch(
             Text("Find the hidden word!")
         }
 
-        GameClue()
+        Text("Clue: ${gameUiState.currentClue}")
+        Text("Answer: ${gameUiState.currentAnswer}")
+        Text(gameUiState.jumbleWithInsertedString)
+        Text("Words completed: ${gameUiState.wordsCompleted}" )
+        Text("Skips: ${gameUiState.skipsPerformed}")
+
         JumbledText()
 
         Spacer(modifier = modifier.padding(8.dp))
 
-        Text("Words completed: 0")
-        Text("Skips: 0")
+        GameStatus()
 
         Spacer(modifier = modifier.padding(8.dp))
 
         OutlinedTextField(
             value = text,
-            onValueChange = { text = it },
+            onValueChange = { gameViewModel.updateUserGuess(it) },
             label = { Text("Enter your answer") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            isError = gameUiState.isGuessedWordWrong,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { gameViewModel.checkSubmittedGuess()  }
+            )
         )
 
         Row(
@@ -49,7 +67,7 @@ fun WordSearch(
                 .padding(top = 16.dp),
             horizontalArrangement = Arrangement.SpaceAround
         ){
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(onClick = { gameViewModel.skipWord() }) {
                 Text("Skip")
             }
             OutlinedButton(onClick = { /*TODO*/ }) {
@@ -63,15 +81,18 @@ fun WordSearch(
 
 
 
+
 @Composable
 fun GameClue(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    clue: String
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ) {
-        Text("Clue: Popular pizza topping")
+//        Text("Clue: Popular pizza topping")
+//        Text("Clue: $clue")
     }
 }
 
@@ -83,8 +104,14 @@ fun JumbledText(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ){
-        Text("qwekfjkdnfjsowkcopdpskdlakfjkdnfjsowkcopdpskdlapepperonikfjkdnfjsowkcopdpskdlakfjkdnfjsowkcopdpskdlaqwr")
+//        Text("qwekfjkdnfjsowkcopdpskdlakfjkdnfjsowkcopdpskdlapepperonikfjkdnfjsowkcopdpskdlakfjkdnfjsowkcopdpskdlaqwr")
     }
+}
+
+@Composable
+fun GameStatus() {
+//    Text("Words completed: 0")
+//    Text("Skips: 0")
 }
 
 @Preview
