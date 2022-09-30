2bf3bab136c1e37fafe8437355c4af183120151c
James Henshaw <jimmy.hensh@gmail.com>
2022-09-30 16:00:49 +0100

    add additional clues and answers

--- app/src/main/java/com/example/wordsearch/data/ClueAndAnswer.kt
+++ app/src/main/java/com/example/wordsearch/data/ClueAndAnswer.kt
@@ -4,7 +4,22 @@ package com.example.wordsearch.data
 val allCluesAndAnswers: List<Pair<String,String>> =
     listOf(
         Pair("Famous pizza topping","pepperoni"),
-        Pair("Smelly cheese","limburger"),
+        Pair("A cheese known for being very smelly","limburger"),
         Pair("Hot continent","africa"),
         Pair("Capital of Hawaii","honolulu"),
+        Pair("Largest state in America","alaska"),
+        Pair("Sport that uses a shuttlecock","badminton"),
+        Pair("A country containing all 5 vowels","mozambique"),
+        Pair("Colour of the seats in the House of Commons","green"),
+        Pair("The science concerned with the origin, evolution, and structure of the earth","geology"),
+        Pair("The colour of neutral litmus paper","purple"),
+        Pair("Main colour of Fred Flintstone's shirt","orange"),
+        Pair("West Bromwich Albion are a team who play this sport","football"),
+        Pair("English city which is often abbreviated to Soton","southampton"),
+        Pair("The alias of the mole in Reservoir Dogs, Mr...","orange"),
+        Pair("Bond film starring Pierce Brosnan","goldeneye"),
+        Pair("Lead singer of 'Slade', Noddy...","holder"),
+        Pair("The drummer from The Beatles, Ringo...","starr"),
+        Pair("Board game which contains 'Chance' and 'Community Chest'","monopoly"),
+        Pair("The name of Ron's rat in Harry Potter","scabbers")
     )
